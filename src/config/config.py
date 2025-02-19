e2a6910421a256ef9084c3ea0c2a4b4dac44922b
Mojesh Thorati <mojesh.t@wrldc.local>
2025-02-19 16:43:04 +0530

    git ignore added

--- src/config/config.py
+++ src/config/config.py
@@ -2,7 +2,7 @@ import json
 
 #this function reads and returns config.json 
 def load_config():
-    with open(r'src\\config\\config.json') as f:
+    with open(r'config.json') as f:
         data = json.load(f)
         return dict(data)
 
